919f419c3afb917006a87225cfb31ccaa6741250
Ximena Meyzen <xmeyzzzenx@gmail.com>
2026-02-11 00:04:52 +0100

    FoodieApp: configuración base de datos Room

---
+++ app/src/main/java/com/ximena/foodieapp/data/local/database/FoodieDatabase.kt
@@ -0,0 +1,18 @@
+package com.ximena.foodieapp.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.ximena.foodieapp.data.local.dao.MealPlanDao
+import com.ximena.foodieapp.data.local.dao.RecipeDao
+import com.ximena.foodieapp.data.local.entity.MealPlanEntity
+import com.ximena.foodieapp.data.local.entity.RecipeEntity
+
+@Database(
+    entities = [RecipeEntity::class, MealPlanEntity::class],
+    version = 1
+)
+abstract class FoodieDatabase : RoomDatabase() {
+
+    abstract fun recipeDao(): RecipeDao
+    abstract fun mealPlanDao(): MealPlanDao
+}
